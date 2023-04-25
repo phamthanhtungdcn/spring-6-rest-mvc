c056f426940e4eeee2393437443b051f3ca1bdde
Nemo Bruke <phamthanhtungdcn@gmail.com>
2023-04-25 17:09:43 +0700

    commit Customer

---
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -0,0 +1,55 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Customer;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private Map<UUID, Customer> customerMap;
+
+    public CustomerServiceImpl() {
+        this.customerMap = new HashMap<>();
+
+        Customer customer1 = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("Nemo")
+                .version(112)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer2 = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("Tomyta")
+                .version(345)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer3 = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("David")
+                .version(897)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        customerMap.put(customer1.getId(), customer1);
+        customerMap.put(customer2.getId(), customer2);
+        customerMap.put(customer3.getId(), customer3);
+    }
+
+    @Override
+    public List<Customer> listCustomers(){
+        return new ArrayList<>(customerMap.values());
+    }
+
+    @Override
+    public Customer getCustomerById(UUID id){
+        return customerMap.get(id);
+    }
+}
